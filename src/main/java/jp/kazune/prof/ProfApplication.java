c29983f1542a9bb7bc721a41eed5f7c5a611b2f4
Kazune Iga <kazune.iga@gmail.com>
2021-03-14 23:47:27 +0900

    add

---
+++ src/main/java/jp/kazune/prof/ProfApplication.java
@@ -0,0 +1,13 @@
+package jp.kazune.prof;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jp/kazune/prof/ProfApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.kazune.prof;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
